600a3dc4b1748785187a9505ac5e2e052b1cd1de
Stylie-cmd <2236597239@qq.com>
2019-12-27 14:30:57 +0800

    zkj第三次更新

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/Login.vue'
+import Home from '../components/Home.vue'
 Vue.use(VueRouter)
 
 const routes = [{
@@ -8,9 +9,22 @@ const routes = [{
 },{
   path: '/login',
   component: Login
-}]
+},{
+  path:'/home',component:Home}]
 const router = new VueRouter({
   routes
 })
 
+
+//挂载路由导航守卫
+router.beforeEach((to,from,next)=>{
+  //to 将要访问的路径
+  //from 代表从哪个路径跳转而来
+  //next 函数 表示放行
+  if(to.path === '/login') return next();
+  //获取token
+  const tokenStr = window.sessionStorage.getItem('token')
+  if(!tokenStr) return next('/login')
+  next()
+})
 export default router
